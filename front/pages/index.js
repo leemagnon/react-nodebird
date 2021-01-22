e9eb48b2b4824d9ca6955fa393309a63734ddf3e
Rat2 <leemagnon@naver.com>
2021-01-22 11:22:48 +0900

    next 설치, 컴포넌트 생성
    1. npm i next@9 react react-dom
    2. pages 폴더 생성

---
+++ front/pages/index.js
@@ -0,0 +1,15 @@
+// import React from 'react'; <- Next는 이 구문이 필요 없다.
+
+const Home = () => {
+    return (
+        <div>Hello, Next!</div>
+    );
+};
+
+export default Home;
+
+/**
+ * pages 폴더는 무조건 이름이 pages 여야 한다. 
+ * 그래야 nextjs가 pages 폴더를 인식하고, 그 안에 있는 파일들을 개별적인 페이지 컴포넌트로 만들어 준다.
+ * pages 안에 들어 있어야 코드 스플리팅이 된다.
+ */
