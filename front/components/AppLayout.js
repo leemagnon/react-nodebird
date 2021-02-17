ca3f8654da32ae9ba0dd9500bae7f4dead801e12
Rat2 <leemagnon@naver.com>
2021-02-17 20:57:59 +0900

    styled-components 사용, UserProfile 컴포넌트 추가

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import styled from 'styled-components';
 import { Menu, Input, Row, Col } from 'antd';
 
 import UserProfile from '../components/UserProfile';
 import LoginForm from '../components/LoginForm';
 
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
+
 const AppLayout = ({ children }) => {
   const [IsLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
       <Menu mode="horizontal">
@@ -22,7 +28,7 @@ const AppLayout = ({ children }) => {
           </Link>
         </Menu.Item>
         <Menu.Item>
-          <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+          <SearchInput enterButton />
         </Menu.Item>
         <Menu.Item>
           <Link href="/signup">
@@ -32,7 +38,11 @@ const AppLayout = ({ children }) => {
       </Menu>
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          {IsLoggedIn ? <UserProfile /> : <LoginForm />}
+          {IsLoggedIn ? (
+            <UserProfile setIsLoggedIn={setIsLoggedIn} />
+          ) : (
+            <LoginForm setIsLoggedIn={setIsLoggedIn} />
+          )}
         </Col>
         <Col xs={24} md={12}>
           {children}
--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -1,8 +1,18 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import { Form, Input, Button } from 'antd';
 import Link from 'next/link';
+// styled-components 는 페이지에서 어떤 컴포넌트가 렌더링 됐는지 추적해서 style을 주입한다.
+import styled from 'styled-components';
 
-const LoginForm = () => {
+const ButtonWrapper = styled.div`
+  margin-top: 10px;
+`;
+
+const FormWrapper = styled(Form)`
+  padding: 10px;
+`;
+
+const LoginForm = ({ setIsLoggedIn }) => {
   const [Id, setId] = useState('');
   const [Password, setPassword] = useState('');
 
@@ -10,12 +20,19 @@ const LoginForm = () => {
     setId(e.target.value);
   }, []);
 
+  // useCallback -> 함수 캐싱
   const onChangePassword = useCallback(e => {
     setPassword(e.target.value);
   }, []);
 
+  const onSubmitForm = useCallback(() => {
+    console.log(Id, Password);
+    setIsLoggedIn(true);
+  }, [Id, Password]);
+
+  // Virtual DOM
   return (
-    <Form>
+    <FormWrapper onFinish={onSubmitForm}>
       <div>
         <label htmlFor="user-id">아이디</label>
         <br />
@@ -32,7 +49,7 @@ const LoginForm = () => {
           required
         />
       </div>
-      <div>
+      <ButtonWrapper>
         <Button type="primary" htmlType="submit" loading={false}>
           로그인
         </Button>
@@ -41,9 +58,19 @@ const LoginForm = () => {
             <Button>회원가입</Button>
           </a>
         </Link>
-      </div>
-    </Form>
+      </ButtonWrapper>
+    </FormWrapper>
   );
 };
 
 export default LoginForm;
+
+/* 리렌더링 최적화를 위해 styled-components를 사용하고 싶지 않으면,
+    useMemo를 이용하여 같은 객체를 유지하게 만들 수도 있다.
+  
+    useMemo -> 값을 캐싱
+
+    const style = useMemo(() => ({ marginTop: 10 }, []));
+
+    <div style={style}> ... </div> 이런 식으로 넣을 수 있다.
+  */
